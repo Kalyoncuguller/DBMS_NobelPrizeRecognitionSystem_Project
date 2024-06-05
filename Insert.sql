54275e9376b2c8c9ba137b53e7ee064ee6c3c6ab
Güller Kalyoncu <118815433+Kalyoncuguller@users.noreply.github.com>
2024-06-05 16:29:30 +0300

    Add files via upload

---
+++ Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO NOMINEES
+VALUES (16, 'Theo James', '1984-12-16', 'English', 'Physics');
+
+INSERT INTO AWARDS
+VALUES (8, 'Maths', 'Nobel Prize in Maths');
+
+INSERT INTO GLOBAL_ORGANIZATIONS
+VALUES (7, 'The US Nobel Committee', 'The US', 'infodbry@nobel.no');
+
+INSERT INTO COMMUNITE(number, member, criteria)
+VALUES 	(11, 'Ryan Gosling', 'Criteria L');
